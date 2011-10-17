a8a3278b0245e02a39c2c917da786f7ea4fc96a9
yiminghe <yiminghe@gmail.com>
2011-10-17 11:19:54 +0800

    update

--- checkcode/checkcode.js
+++ checkcode/checkcode.js
@@ -3,10 +3,14 @@
  * http://www.phon.ucl.ac.uk/home/mark/audio/play.htm
  * https://developer.mozilla.org/En/Using_audio_and_video_in_Firefox
  * https://developer.mozilla.org/En/Media_formats_supported_by_the_audio_and_video_elements
+ * http://kathymarks.com/archives/2005/09/embedding_windows_media_and_quicktime_video_on_a_web_page.html
  * @author yiminghe@gmail.com
  */
 (function() {
 
+    function isWin(){
+        return navigator.userAgent.indexOf("Windows")!=-1;
+    }
     var S = KISSY,
         doc = document,
         DOM = S.DOM,
@@ -17,7 +21,9 @@
             '<audio preload="auto" autoplay="autoplay"></audio>' :
             // ie
             // can only be put under body ??
-            '<bgsound></bgsound>';
+            (S.UA.ie?'<bgsound></bgsound>':
+            // old firefox , use wnidows media player in windows and quicktime in others
+            '<embed '+(isWin()?'type="application/x-mplayer2"':'')+' autostart="true" hidden="true"></embed>');
 
     // ie6 need this ??
     function refreshPlayer() {
